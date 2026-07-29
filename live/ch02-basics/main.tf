7bdddec98d06492dfcccedd4011d3c32922c95fc
gbblnchrd <gabriel.blanchard.ucf@gmail.com>
2026-07-28 19:44:12 -0600

    Changing single EC2 instance to an Autoscaling Group. Using data sources to grab default VPC and subnet information to use in the ASG.

--- live/ch02-basics/main.tf
+++ live/ch02-basics/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!/bin/bash
@@ -13,10 +13,23 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-  user_data_replace_on_change = true
+  # Below is required when using a launch config with an ASG
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -29,6 +42,17 @@ resource "aws_security_group" "instance" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }  
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+      name = "vpc-id"
+      values = [data.aws_vpc.default.id]
+    }
 }
 
 variable "server_port" {
